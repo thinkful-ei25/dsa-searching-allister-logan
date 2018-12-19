e9560ecae7baa52f08e4fa50161bb1298e801b2c
Logan Wang <loganxwang@hotmail.com>
2018-12-19 09:47:39 -0800

    completed binary search

--- searching-app/src/App.js
+++ searching-app/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { linearSearch } from './search'
+import { linearSearch, binarySearch } from './search'
 
 console.log(linearSearch);
 
 class App extends Component {
 
   constructor(props) {
-      super(props)  
-      this.state = {
+    super(props)
+    this.state = {
       dataset: [89, 30, 25, 32, 72, 70, 51, 42],
       searchTerm: null,
       result: null
     }
-    
+
   }
 
 
@@ -22,12 +22,19 @@ class App extends Component {
     e.preventDefault();
     console.log(this.state.searchTerm)
     console.log(this.state.dataset)
-    this.setState( 
-    {
-     result: linearSearch(Number(this.state.searchTerm), this.state.dataset)
-    })
+    this.setState(
+      {
+        result: linearSearch(Number(this.state.searchTerm), this.state.dataset)
+      })
   }
 
+  onClickBinary(e) {
+    e.preventDefault();
+    let sortedData = this.state.dataset.sort();
+    this.setState({
+      result: binarySearch(Number(this.state.searchTerm), sortedData)
+    })
+  }
   render() {
     return (
       <div>
@@ -35,9 +42,10 @@ class App extends Component {
           <label htmlFor="searchTerm">Enter search number:</label>
           <input onChange=
             {e => this.setState({
-                searchTerm: e.target.value
-              })} type="number" id="searchTerm"></input>
-          <button onClick={(e => this.onClick(e))}>Submit</button>
+              searchTerm: e.target.value
+            })} type="number" id="searchTerm"></input>
+          <button onClick={(e => this.onClick(e))}>Linear Search</button>
+          <button onClick={(e => this.onClickBinary(e))}>Binary Search</button>
         </form>
         <div>
           {this.state.result}
--- searching-app/src/search.js
+++ searching-app/src/search.js
@@ -9,3 +9,28 @@ export function linearSearch(searchTerm, arr) {
     return -1;
 }
 
+export function binarySearch(searchTerm, arr, start = 0, end = arr.length - 1, counter = 0) {
+    if (start > end) {
+        counter++;
+        return -1;
+    }
+    let middle = Math.floor((start + end) / 2);
+    // console.log('start', start);
+    // console.log('end', end);
+    // console.log(counter);
+    if (arr[middle] === searchTerm) {
+        counter++;
+        return `Found in ${counter}`;
+    }
+    else if (arr[middle] < searchTerm) {
+        counter++;
+        return binarySearch(searchTerm, arr, middle + 1, end, counter);
+    }
+    else if (arr[middle] > searchTerm) {
+        counter++;
+        return binarySearch(searchTerm, arr, start, middle - 1, counter);
+    }
+}
+
+// console.log(binarySearch(89, [25, 30, 32, 42, 51, 70, 72, 89]));
+
